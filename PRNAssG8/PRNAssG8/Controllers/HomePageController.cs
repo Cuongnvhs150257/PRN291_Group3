f4aab3b6583d69a99acc399feb70d2e24a55adf7
Cuongnvhs150257 <97275308+Cuongnvhs150257@users.noreply.github.com>
2022-07-08 14:22:52 +0700

    build cshtml

---
+++ PRNAssG8/PRNAssG8/Controllers/HomePageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PRNAssG8.Models;
+
+namespace PRNAssG8.Controllers
+{
+    public class HomePageController : Controller
+    {
+        PRN1Context db = new PRN1Context();
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
